840e8864ef117e42dd0126f0977636f5df8934c8
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-04-16 19:54:10 +0530

    Add files via upload

---
+++ c++ dynamic declaration of variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+	double n,s;
+	cout<<"enter value:";
+	cin>>n;
+	s=sqrt(n);
+	cout<<"the square root of"<<n<<"is"<<s;
+
+return 0;
+}
